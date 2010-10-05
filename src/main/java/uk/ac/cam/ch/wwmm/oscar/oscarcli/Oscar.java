5b725a3cba29ac476acb8993b09c1c8f5cd8a48e
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-05 13:08:49 +0200

    Initial checkin

---
+++ src/main/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/Oscar.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.ch.wwmm.oscar.oscarcli;
+
+/**
+ * @author egonw
+ */
+public class Oscar 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
