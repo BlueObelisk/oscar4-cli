e11495cecb3e718afb99d410ffbfc90440c1f937
Egon Willighagen <egon.willighagen@gmail.com>
2010-10-05 13:08:49 +0200

    Initial checkin

---
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/OscarTest.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.ch.wwmm.oscar.oscarcli;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author egonw
+ */
+public class OscarTest {
+
+	@Test public void testConstructor() {
+		Assert.assertNotNull(new Oscar());
+	}
+
+}
