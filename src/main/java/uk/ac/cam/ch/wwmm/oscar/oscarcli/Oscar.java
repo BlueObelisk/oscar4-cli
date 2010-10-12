2c14dbef2e20dd245cc129efbbedda87f43aa4c0
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-12 13:32:32 +0100

    A full Oscar4 pipeline

--- src/main/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/Oscar.java
+++ src/main/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/Oscar.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 import nu.xom.Builder;
 import nu.xom.Document;
+import nu.xom.Element;
+import uk.ac.cam.ch.wwmm.opsin.NameToStructure;
+import uk.ac.cam.ch.wwmm.opsin.NameToStructureException;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult.OPSIN_RESULT_STATUS;
 import uk.ac.cam.ch.wwmm.oscar.document.NamedEntity;
 import uk.ac.cam.ch.wwmm.oscar.document.ProcessingDocument;
 import uk.ac.cam.ch.wwmm.oscar.document.ProcessingDocumentFactory;
+import uk.ac.cam.ch.wwmm.oscar.document.Token;
 import uk.ac.cam.ch.wwmm.oscar.document.TokenSequence;
 import uk.ac.cam.ch.wwmm.oscarMEMM.MEMMRecogniser;
 import uk.ac.cam.ch.wwmm.oscarMEMM.memm.document.Tokeniser;
@@ -17,15 +23,43 @@ import uk.ac.cam.ch.wwmm.oscarMEMM.memm.document.Tokeniser;
  */
 public class Oscar {
 
-	public Oscar() {};
+	private NameToStructure nameToStructure;
+	
+	public Oscar() throws NameToStructureException {
+		nameToStructure = NameToStructure.getInstance();
+	};
 	
 	public static void main(String[] args) throws Exception {
-		String input = args[0]; // yes, yes, very dirty...
+		String input = "some aspirin would be nice";
+		if (args.length > 0) {
+			StringBuilder builder = new StringBuilder();
+			for (String arg : args) builder.append(arg);
+			input = builder.toString();
+		}
 		Oscar oscar = new Oscar();
 		input = oscar.normalize(input);
 		List<TokenSequence> tokens = oscar.tokenize(input);
 		List<NamedEntity> entities = oscar.recognizeNamedEntities(tokens);
-		// List<IMolecule> molecules = oscar.resolveNamedEntities(entities);
+		for (NamedEntity entity : entities) {
+			System.out.println(entity);
+		}
+		List<Element> molecules = oscar.resolveNamedEntities(entities);
+		for (Element mol : molecules) {
+			System.out.println(mol.getLocalName());
+		}
+	}
+
+	private List<Element> resolveNamedEntities(List<NamedEntity> entities) {
+		List<Element> cmlMols = new ArrayList<Element>();
+		for (NamedEntity entity : entities) {
+			System.out.println("Entity: " + entity.getSurface());
+			OpsinResult result = nameToStructure.parseChemicalName(
+				entity.getSurface(), false
+			);
+			if (result.getStatus() == OPSIN_RESULT_STATUS.SUCCESS)
+				cmlMols.add(result.getCml());
+		}
+		return cmlMols;
 	}
 
 	public List<TokenSequence> tokenize(String input) throws Exception {
@@ -39,6 +73,10 @@ public class Oscar {
 				doc, true, false, false
 			);
 		List<TokenSequence> tokenSequences = procDoc.getTokenSequences();
+		for (TokenSequence tokens : tokenSequences) {
+			for (Token token : tokens.getTokens())
+				System.out.println("token: " + token.getValue());
+		}
 		return tokenSequences;
 	}
 
