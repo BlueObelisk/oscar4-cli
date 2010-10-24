cd105d92a511cf8f77034c14824504b101426a4a
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-24 15:42:03 +0200

    Added missing unit test

--- src/test/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/OscarTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/oscarcli/OscarTest.java
@@ -3,10 +3,10 @@ package uk.ac.cam.ch.wwmm.oscar.oscarcli;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.ac.cam.ch.wwmm.opsin.NameToStructureException;
+import uk.ac.cam.ch.wwmm.oscar.document.NamedEntity;
 import uk.ac.cam.ch.wwmm.oscar.document.TokenSequence;
 
 /**
@@ -31,6 +31,15 @@ public class OscarTest {
 		List<TokenSequence> tokens = oscar.tokenize("This is a simple input string with benzene.");
 		Assert.assertNotNull(tokens);
 		Assert.assertNotSame(0, tokens.size());
+	}
+
+	@Test
+	public void testRecognizeNamedEntities() throws Exception {
+		Oscar oscar = new Oscar();
+		List<TokenSequence> tokens = oscar.tokenize("This is a simple input string with benzene.");
+		List<NamedEntity> entities = oscar.recognizeNamedEntities(tokens);
+		Assert.assertNotNull(entities);
+		Assert.assertEquals(1, entities.size());
 	}
 
 	@Test public void testMain() throws Exception {
